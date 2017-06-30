809e4e965fc72f8ea354fecb19d134558c45dbe2
Edilberto Salazar <mosh086@gmail.com>
2017-06-30 18:09:37 -0500

    payments 6/30

--- api/db.js
+++ api/db.js
@@ -3,8 +3,10 @@ var pool  = null;
 exports.connect = function() {
   pool = mysql.createPool({
     host     : 'localhost',
-    user     : 'acanijo',
-    password : 'M3gamente',
+    //user     : 'acanijo',
+    //password : 'M3gamente',
+    user     : 'root',
+    password : 'Toolmosh_86',
     database : 'guardartododb',
     dateStrings:true
   });
--- client/app/components/payment/payment.controller.js
+++ client/app/components/payment/payment.controller.js
@@ -27,9 +27,18 @@ class PaymentController {
 
   }
 
-  remove(id, name) {
+  remove(id) {
     let self = this;
-
+    this._PaymentService.remove(id)
+      .then((res) => {
+        self._toastr.success(`El pago fue eliminado correctamente`);
+        self.searchPayments();
+      },
+        (err) => {
+          console.log('error: ' + err);
+          self._toastr.error(`Error ${err.message}`);
+        }
+      )
   }
 
   searchPayments() {
